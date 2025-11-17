7922b2f5b83ed502368760afeb358dd3667194f2
Pascal Martin <pascal.fb.martin@gmail.com>
2025-11-16 17:18:05 -0800

    Add basic light control

--- README.md
+++ README.md
@@ -29,9 +29,23 @@ The lights schedule can be edited from the HouseLights web interface.
 
 The list of control points is dynamically retrieved from the House services implementing the `control` interface.
 
-This service supports a graphic map display to control the lights.  That map display requires the presence of a user created `floorplan.svg` file in `/var/lib/house/lights`. This SVG file is typically created using Inkscape. Each SVG element that reflects the state of a point must not have any `stroke` attribute and must have an `id` attribute matching the ID of the point (except for ' ' replaced with '_').
+This service supports a graphic map display to control the lights.  That map display requires the presence of a user created `floorplan.svg` file in `/var/lib/house/lights`. This SVG file is typically created using Inkscape (see later).
 
 An HTML page is automatically generated based on this `floorplan.svg` file and cached as `/var/cache/house/lights/mapbody.html`: this later file must be deleted after each change to a `/var/lib/house/lights` file.
+
+## Creating a floor plan using Inkscape
+
+A floor plan SVG can be created using Inkscape, but a few conventions must be followed:
+
+* Define a background color for the page in `Document Properties`. It is recommended to use `#355b1eff`, which is the background used in the web UI.
+
+* Each SVG element that reflects the state of a point must not have the `stroke` attribute and must have an `id` attribute matching the name of the point (except for ' ' replaced with '_').
+
+* Symbols can be used, but the same "no stroke" rule applies.
+
+* Controls are supported using the `onclick` attribute. This attribute must call the `controlClick` function with the name of the point as its parameter. The `onclick` attribute cannot be assigned to a symbol item (the name of the point is not known at this stage).
+
+* The Inkscape project must be exported as `plain SVG`, and this plain SVG file is to be installed as `floorplan.svg` in `/var/lib/house/lights`.
 
 ## Panel
 
--- public/animate.js
+++ public/animate.js
@@ -1,26 +1,49 @@
 // Animate.js: change the properties of HTML/SVG elements based on
 // the current status of points returned by the server.
 
-var UpdateUrl = "/status";
+var RootUrl = "";
 
 var KnownState = new Object();
+var ChangeToReverse = new Object();
+
+ChangeToReverse["on"] = "off";
+ChangeToReverse["off"] = "on";
 
 function lightsUpdateStatus (response) {
 
     var plugs = response.lights.plugs;
     for (var i = 0; i < plugs.length; i++) {
-        var tag = plugs[i].name.replace (/ /g,'_')
+        var name = plugs[i].name;
+        var tag = name.replace (/ /g,'_')
         var state = plugs[i].state;
-        if (KnownState[tag] && (KnownState[tag] === state)) continue;
+        if (KnownState[name] && (KnownState[name] === state)) continue;
         var symbol = document.getElementById (tag);
         if (symbol) symbol.setAttribute ("class", state);
-        KnownState[tag] = state;
+        KnownState[name] = state;
     }
+}
+
+function lightsControlRequest (id, state) {
+   if (!state) return;
+   var command = new XMLHttpRequest();
+   command.open
+      ("GET", RootUrl+"/set?device="+id+"&state="+state+"&cause=MANUAL");
+   command.onreadystatechange = function () {
+      if (command.readyState === 4 && command.status === 200) {
+          lightsUpdateStatus (JSON.parse(command.responseText));
+      }
+   }
+   command.send(null);
+}
+
+function controlClick (id) {
+    if (KnownState[id])
+       lightsControlRequest (encodeURIComponent(id), ChangeToReverse[KnownState[id]]);
 }
 
 function lightsStatus () {
     var command = new XMLHttpRequest();
-    command.open("GET", UpdateUrl);
+    command.open("GET", RootUrl+"/status");
     command.onreadystatechange = function () {
         if (command.readyState === 4 && command.status === 200) {
             var response = JSON.parse(command.responseText);
@@ -31,7 +54,7 @@ function lightsStatus () {
 }
 
 function animateStart (path) {
-   UpdateUrl = path + '/status';
+   RootUrl = path;
    lightsStatus();
    setInterval (lightsStatus, 1000);
 }
